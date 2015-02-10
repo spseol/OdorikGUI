79fc97f7866fb092b9cdce2ac2a5dbead3c61536
Grofy <grofy1@gmail.com>
2015-02-10 11:31:40 +0100

    zacatky_speeddials

--- seminarni_prace_grofek.py
+++ seminarni_prace_grofek.py
@@ -6,14 +6,23 @@ Created on Thu Jan 29 21:04:50 2015
 """
 import urllib
 import Tkinter as tk
+##############################################################################
+##############################################################################
+
+
+def vypsat(kontakty):
+     return kontakty["shortcut"], kontakty["name"], kontakty["number"]
 
 
 def prihlasit():
     jmeno = jmeno_entry.get()
     heslo = heslo_entry.get()
     udaje = urllib.urlencode({"user": jmeno, "password": heslo})            ## přihlašovací údaje jako parametr
-    kredit_mezikrok = urllib.urlopen("https://www.odorik.cz/api/v1/balance?"+udaje) ## zjistí kredit
+    kredit_mezikrok = urllib.urlopen("https://www.odorik.cz/api/v1/balance?"+udaje) ## získání kreditu
     kredit = kredit_mezikrok.read()         ## kredit jako řetězec
+    kontakty_mezikrok = urllib.urlopen("https://www.odorik.cz/api/v1/speed_dials.json?"+udaje) ##získání kontaktů
+    kontakty_dalsi_mezikrok = kontakty_mezikrok.read()
+    kontakty = json.loads(kontakty_dalsi_mezikrok, object_hook=vypsat)          ##kontakty jako seznam
     if kredit == "error authentication_failed":     ##špatně zadané údaje
         chybne_prihl_okno = tk.Tk()
         chybne_prihl_okno.title("Chyba!!")
@@ -21,10 +30,26 @@ def prihlasit():
         chybne_prihl_label.pack()
         chybne_prihl_okno.mainloop()
     else:                                           ## dobře zadané údaje
-        hl_okno.quit()
+        """hl_okno.quit()"""                        ## proč nefunguje?! 
         prihl_okno = tk.Tk()
-        kredit_label = tk.Label(prihl_okno, text="Váš kredit je: "+kredit+"Kč")
-        kredit_label.pack()
+        kredit_label = tk.Label(prihl_okno, text="Váš kredit je: "+kredit+"Kč")   ##vypisuje kredit
+        kredit_label.grid(columnspan=3)
+        zkratka_label = tk.Label(prihl_okno, text="Zkratka")                    ##záhlaví callbacku
+        zkratka_label.grid(row=1)
+        jmeno_label = tk.Label(prihl_okno, text=u"Jméno")
+        jmeno_label.grid(row=1, column=1)
+        cislo_label = tk.Label(prihl_okno, text=u"Číslo")
+        cislo_label.grid(row=1, column=2)
+        zkratka_vstup = tk.Entry(prihl_okno)                                    ##vstupy 
+        zkratka_vstup.grid(row=2)
+        jmeno_vstup = tk.Entry(prihl_okno)
+        jmeno_vstup.grid(row=2, column=1)
+        cislo_vstup = tk.Entry(prihl_okno)
+        cislo_vstup.grid(row=2, column=2)
+        doplnit_button = tk.Button(prihl_okno, text="Doplnit")                  ## tlacitka pod vstupy
+        doplnit_button.grid(row=3)
+        callback_button = tk.Button(prihl_okno, text="Objednat callback")
+        callback_button.grid(row=3, columnspan=2, column=2)
         prihl_okno.mainloop()
 
 
@@ -49,6 +74,8 @@ def vyplnit_udaje():
     jmeno_entry.insert(tk.END, jmeno)
     heslo_entry.insert(tk.END, heslo)
 
+##############################################################################
+##############################################################################
 
 hl_okno = tk.Tk()                   ##Úvodní přihlašovací okno
 hl_okno.title("Odorik.cz")
