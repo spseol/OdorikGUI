f3a1e52e5434cd57af43f34f43ad3ebddd1c2fcc
Grofy <grofy1@gmail.com>
2015-02-18 19:25:09 +0100

    zkouška záhlaví, nefunguje (prozatím)
    funkce na doplnění vstupů probíhá při spuštění okna

--- seminarni_prace_grofek.py
+++ seminarni_prace_grofek.py
@@ -4,6 +4,7 @@ Created on Thu Jan 29 21:04:50 2015
 
 @author: Tom
 """
+import json
 import urllib
 import Tkinter as tk
 ##############################################################################
@@ -14,15 +15,17 @@ def vypsat(kontakty):
      return kontakty["shortcut"], kontakty["name"], kontakty["number"]
 
 
+
 def prihlasit():
-    jmeno = jmeno_entry.get()
+    prihl_jmeno = prihl_jmeno_entry.get()
     heslo = heslo_entry.get()
-    udaje = urllib.urlencode({"user": jmeno, "password": heslo})            ## přihlašovací údaje jako parametr
-    kredit_mezikrok = urllib.urlopen("https://www.odorik.cz/api/v1/balance?"+udaje) ## získání kreditu
+    udaje = urllib.urlencode({"user": prihl_jmeno, "password": heslo})            ## přihlašovací údaje jako parametr
+    kredit_mezikrok = urllib.urlopen("https://www.odorik.cz/api/v1/balance?"+udaje)  ## získání kreditu
     kredit = kredit_mezikrok.read()         ## kredit jako řetězec
-    kontakty_mezikrok = urllib.urlopen("https://www.odorik.cz/api/v1/speed_dials.json?"+udaje) ##získání kontaktů
+    kontakty_mezikrok = urllib.urlopen("https://www.odorik.cz/api/v1/speed_dials.json?"+udaje)   ##získání kontaktů
     kontakty_dalsi_mezikrok = kontakty_mezikrok.read()
-    kontakty = json.loads(kontakty_dalsi_mezikrok, object_hook=vypsat)          ##kontakty jako seznam
+    kontakty = json.loads(kontakty_dalsi_mezikrok, object_hook=vypsat)          ##kontakty jako 3-členné ntice v seznamu
+    print kontakty
     if kredit == "error authentication_failed":     ##špatně zadané údaje
         chybne_prihl_okno = tk.Tk()
         chybne_prihl_okno.title("Chyba!!")
@@ -30,7 +33,7 @@ def prihlasit():
         chybne_prihl_label.pack()
         chybne_prihl_okno.mainloop()
     else:                                           ## dobře zadané údaje
-        """hl_okno.quit()"""                        ## proč nefunguje?! 
+        """hl_okno.quit()"""                        ## proč nefunguje?!
         prihl_okno = tk.Tk()
         kredit_label = tk.Label(prihl_okno, text="Váš kredit je: "+kredit+"Kč")   ##vypisuje kredit
         kredit_label.grid(columnspan=3)
@@ -40,21 +43,68 @@ def prihlasit():
         jmeno_label.grid(row=1, column=1)
         cislo_label = tk.Label(prihl_okno, text=u"Číslo")
         cislo_label.grid(row=1, column=2)
-        zkratka_vstup = tk.Entry(prihl_okno)                                    ##vstupy 
-        zkratka_vstup.grid(row=2)
-        jmeno_vstup = tk.Entry(prihl_okno)
-        jmeno_vstup.grid(row=2, column=1)
-        cislo_vstup = tk.Entry(prihl_okno)
-        cislo_vstup.grid(row=2, column=2)
-        doplnit_button = tk.Button(prihl_okno, text="Doplnit")                  ## tlacitka pod vstupy
+        zkratka_entry = tk.Entry(prihl_okno)                                    ##vstupy
+        zkratka_entry.grid(row=2)
+        jmeno_entry = tk.Entry(prihl_okno)
+        jmeno_entry.grid(row=2, column=1)
+        cislo_entry = tk.Entry(prihl_okno)
+        cislo_entry.grid(row=2, column=2)
+###########################NEFUNGUJE############################################
+        doplnit_button = tk.Button(prihl_okno, text="Doplnit", command=doplnit(zkratka_entry, cislo_entry, jmeno_entry, kontakty))                  ## tlacitka pod vstupy
         doplnit_button.grid(row=3)
+################################################################################
         callback_button = tk.Button(prihl_okno, text="Objednat callback")
         callback_button.grid(row=3, columnspan=2, column=2)
         prihl_okno.mainloop()
+        
 
-
-def ulozit_udaje():             # uloží údaje
+def doplnit(zkratka_entry, cislo_entry, jmeno_entry, kontakty):  ## doplní ostatní údaje do entry v hlavičce
     jmeno = jmeno_entry.get()
+    cislo = cislo_entry.get()
+    zkratka = zkratka_entry.get()
+    if len(zkratka) > 0:            ## pokud je vyplněna zkratka doplní jmeno a cislo
+        for i in kontakty:
+            if str(i[0]) == zkratka:
+                jmeno_entry.delete(0, END)
+                jmeno_entry.insert(INSERT, i[1])
+                cislo_entry.delete(0, END)
+                cislo_entry.insert(INSERT, i[2])
+            else:
+                print "CHYBA"
+    elif len(jmeno) > 0:            ##doplnene jmeno .. doplni zkratku a cislo
+        for i in kontakty:
+            if str(i[1]) == jmeno:
+                zkratka_entry.delete(0, END)
+                zkratka_entry.insert(INSERT, i[0])
+                cislo_entry.delete(0, END)
+                cislo_entry.insert(INSERT, i[2])
+            else:
+                print "CHYBA"
+    elif len(cislo) > 0:            ## napodobe ale je vyplnene cislo
+        for i in kontakty:
+            if str(i[2]) == cislo:
+                zkratka_entry.delete(0, END)
+                zkratka_entry.insert(INSERT, i[0])
+                jmeno_entry.delete(0, END)
+                jmeno_entry.insert(INSERT, i[1])
+            else:
+                print "CHYBA"
+    elif len(jmeno) > 0:
+         for i in kontakty:
+            if str(i[3]) == jmeno:
+                zkratka_entry.delete(0, END)
+                zkratka_entry.insert(INSERT, i[0])
+                jmeno_entry.delete(0, END)
+                jmeno_entry.insert(INSERT, i[1])
+            else:
+                print "CHYBA"
+    else:
+        print "CHYBAasad"
+#############################################################################
+        
+        
+def ulozit_udaje():             # uloží údaje
+    jmeno = prihl_jmeno_entry.get()
     heslo = heslo_entry.get()
     jmeno_soubor = open("jmeno.txt", "w")
     heslo_soubor = open("heslo.txt", "w")
@@ -69,9 +119,9 @@ def vyplnit_udaje():
     heslo_soubor = open("heslo.txt", "r")
     jmeno = jmeno_soubor.readline()
     heslo = heslo_soubor.readline()
-    jmeno_entry.delete(0, tk.END)
+    prihl_jmeno_entry.delete(0, tk.END)
     heslo_entry.delete(0, tk.END)
-    jmeno_entry.insert(tk.END, jmeno)
+    prihl_jmeno_entry.insert(tk.END, jmeno)
     heslo_entry.insert(tk.END, heslo)
 
 ##############################################################################
@@ -80,11 +130,11 @@ def vyplnit_udaje():
 hl_okno = tk.Tk()                   ##Úvodní přihlašovací okno
 hl_okno.title("Odorik.cz")
 
-jmeno_label = tk.Label(hl_okno, text="Jméno")
-jmeno_label.grid()
+prihl_jmeno_label = tk.Label(hl_okno, text="Jméno")
+prihl_jmeno_label.grid()
 
-jmeno_entry = tk.Entry(hl_okno, justify="center")
-jmeno_entry.grid(row=1)
+prihl_jmeno_entry = tk.Entry(hl_okno, justify="center")
+prihl_jmeno_entry.grid(row=1)
 
 heslo_label = tk.Label(hl_okno, text="Heslo")
 heslo_label.grid(row=2)
